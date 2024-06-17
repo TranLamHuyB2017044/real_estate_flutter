23dd8f6d4af94e0e6120eebb497db2cdd3a93914
HuyLam <tranlamhuy5tn@gmail.com>
2024-06-17 13:39:06 +0700

    fix: fix function getPhoto

--- lib/viewmodels/photo_viewmodel.dart
+++ lib/viewmodels/photo_viewmodel.dart
@@ -1,23 +1,22 @@
 import 'dart:convert';
 
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart';
 import 'package:my_real_estate/models/photo_model.dart';
 
-class PhotoViewModel extends ChangeNotifier {
+class PhotoViewModel {
   final String baseURL = 'https://jsonplaceholder.typicode.com/photos';
 
-  Future<List<Photo>> getPhoto() async {
+  Future<List<Photo>> getPhotos() async {
     Response response = await get(Uri.parse(baseURL));
 
     if (response.statusCode == 200) {
-      final List result = jsonDecode(response.body)['data'];
+      final List result = jsonDecode(response.body);
       return result.map((photo) => Photo.fromJson(photo)).toList();
     } else {
       throw Exception(response.reasonPhrase);
     }
   }
-
-  final photoProvider = Provider<PhotoViewModel>((ref) => PhotoViewModel());
 }
+
+final photoProvider = Provider<PhotoViewModel>((ref) => PhotoViewModel());
